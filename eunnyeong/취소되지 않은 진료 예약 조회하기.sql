e9ff5aac23bfa9719f56cd89ba87149d644b3747
Eunnyeong Choi <41516670+doseee@users.noreply.github.com>
2025-12-29 21:12:00 +0900

    Create 취소되지 않은 진료 예약 조회하기.sql

---
+++ eunnyeong/취소되지 않은 진료 예약 조회하기.sql
@@ -0,0 +1,7 @@
+SELECT a.apnt_no, p.pt_name, p.pt_no, a.mcdp_cd, d.dr_name, a.apnt_ymd
+FROM appointment a, patient p, doctor d
+WHERE date(a.apnt_ymd) = '2022-04-13'
+  AND a.mcdp_cd = 'CS'
+  AND a.apnt_cncl_yn = 'N'
+  and d.dr_id = a.mddr_id and p.pt_no = a.pt_no
+ORDER BY a.apnt_ymd;
